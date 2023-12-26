06dcbaeac91211373288cfedd2a2a7b199344c08
aavendan <aavenda@gmail.com>
2023-12-25 19:09:29 -0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,6 @@ import { MainComponent } from './components/main/main.component';
 export const routes: Routes = [
     { path: '', component: MainComponent},
     { path: 'data', component: AboutComponent},
-    // { path: '**', component: MainComponent},
-    // { path: '', redirectTo: 'main', pathMatch: 'full' },
+    { path: '**', component: MainComponent},
+    { path: '', redirectTo: 'main', pathMatch: 'prefix' },
 ];
